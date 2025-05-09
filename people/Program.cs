7e9847d00c8e9364926381674151e01fb084fc06
Maram suliman <mms.bur@outlook.com>
2025-05-09 16:26:12 +0400

    Add files via upload

---
+++ people/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace project_2__maram
+{
+    internal class Program
+        {
+            static void Main(string[] args)
+            {
+                people people1 = new people( " suliman", 23, " A1234567");
+
+                 Console.WriteLine(people1.Getname());
+                 Console.WriteLine(people1.Getage());  
+                 Console.WriteLine(people1.Getpassword());
+           
+
+
+
+
+            }
+        
+    }
+}
---
+++ people/people.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Xml.Linq;
+namespace project_2__maram
+{
+    public class people
+    {
+        private int id;
+        private string name;
+        private int age;
+        private string password;
+
+
+        public people( string name, int age, string password)
+        {
+
+            
+            this.name = name;
+            this.age = age;
+            this.password = password;
+
+        }
+
+        public String Getname()
+        {
+            return this.name;
+        }
+
+        public int Getage()
+        {
+            return this.age;
+        }
+
+        public String Getpassword()
+        {
+            return this.password;
+        }
+
+
+        public void Setname()
+        {
+            this.name = name;
+        }
+
+        public void Setage()
+        {
+            this.age = age;
+        }
+
+        public void Setpassword()
+        {
+            this.password = password;
+        }
+    }
+
+}
+
+
+
+   
